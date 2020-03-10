69d399dc6c4e8bf51405db364a916e19547e6760
Anam <biancamajikijela95@gmail.com>
2020-03-10 11:15:36 +0200

    data types

---
+++ Data types.py
@@ -0,0 +1,32 @@
+#declaring a string variable
+x = "life choices coding academy"
+#displaying data type of str
+print(type(x))
+print(x[2:5])
+
+#Trying to find out data types of these numbers, whether its a integer/float/complex number
+intnum1 = 5
+intnum2 = 5.8
+intnum3 = 7j
+print(type(intnum1), type(intnum2), type(intnum3))
+
+#checking what data type this is
+mylist = [23, 82, 42, 33]
+mylist.insert(6,9)
+print(mylist.insert)
+
+#This is a set
+cars = {"aston martin","VW","ford","BMW","Nissan"}
+print(type(cars))
+cars1 = {"Madza","toyota","range rover","kia"}
+print(type(cars1))
+
+#Learning about boolean
+late = True
+print(type(late))
+early = False
+print(type(early))
+
+
+mydict = {"1": "Oslin", "2": "kayleen","3":"Nazneen", "4":"AK"}
+print(type(mydict))
